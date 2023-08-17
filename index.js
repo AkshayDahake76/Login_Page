0f0eb970bcbc912ebc57ac1a103b6cf64946115f
Akshay Dahake <dahakeakshay76@gmail.com>
2023-08-17 14:54:18 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,58 @@
 import React, { useState } from "react";
 
-const Login = (props) => {
+const Register = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [rePassword, setRePassword] = useState("");
+  const [name, setName] = useState("");
+  const [passwordMismatch, setPasswordMismatch] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email);
+
+    if (password !== rePassword) {
+      setPasswordMismatch(true);
+      return;
+    }
+
+    if (!validatePassword(password)) {
+      setPasswordError(true);
+      return;
+    }
+
+    console.log("Registration submitted:", name, email, password);
+  };
+
+  const validatePassword = (password) => {
+    const passwordPattern =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,20}$/;
+    return passwordPattern.test(password);
   };
 
   return (
     <div className="auth-form-container">
-      <h2>Login</h2>
-      <form className="login-form" onSubmit={handleSubmit}>
-        <label htmlFor="email">Email</label>
+      <h2>Register</h2>
+      <form className="register-form" onSubmit={handleSubmit}>
+        <label htmlFor="name">Full Name</label>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          placeholder="Full Name"
+          id="name"
+          name="name"
+        />
+        <label htmlFor="email">Email/Phone No</label>
         <input
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          type="email"
-          placeholder="youremail@gmail.com"
+          type="text"
+          placeholder="Email/Phone No"
           id="email"
           name="email"
         />
+        {/* ... other fields */}
         <label htmlFor="password">Password</label>
         <input
           value={password}
@@ -30,19 +61,38 @@ const Login = (props) => {
           placeholder="********"
           id="password"
           name="password"
+          required
         />
-        <button type="submit" className="login-btn ">
-          Log In
+        {passwordError && (
+          <p className="password-error">
+            Password should be 6-20 characters long and include at least one
+            uppercase letter, one lowercase letter, one digit, and one special
+            character.
+          </p>
+        )}
+        {/* ... other fields */}
+        <label htmlFor="rePassword">Re-Enter Password</label>
+        <input
+          value={rePassword}
+          onChange={(e) => setRePassword(e.target.value)}
+          type="password"
+          placeholder="********"
+          id="rePassword"
+          name="rePassword"
+          required
+        />
+        {passwordMismatch && (
+          <p className="password-mismatch">Passwords do not match.</p>
+        )}
+        <button type="submit" className="login-btn">
+          Submit
         </button>
       </form>
-      <button
-        className="link-btn"
-        onClick={() => props.onFormSwitch("register")}
-      >
-        Don't have an account? Register here.
+      <button className="link-btn" onClick={() => props.onFormSwitch("login")}>
+        Already have an account? Login here.
       </button>
     </div>
   );
 };
 
-export default Login;
+export default Register;
